f23d92b24c43838a9426dab14c6b6966115d0dbb
Bernardo Lanza <b.lanza@pinkpeak.com>
2026-03-09 12:50:57 +0100

    details

--- js/custom.js
+++ js/custom.js
@@ -70,6 +70,26 @@ $(function(){
 	        }
 	    });
 
+	    /* award accordion */
+	    $('.award-panel').hide();
+	    $('.award-toggle').on('click', function () {
+	        var $toggle = $(this);
+	        var targetSelector = $toggle.attr('data-target');
+	        var $item = $toggle.closest('.award-item');
+	        var $panel = $(targetSelector);
+	        var isOpen = $item.hasClass('is-open');
+
+	        $('.award-item').removeClass('is-open');
+	        $('.award-toggle').attr('aria-expanded', 'false');
+	        $('.award-panel').stop(true, true).slideUp(220);
+
+	        if (!isOpen) {
+	            $item.addClass('is-open');
+	            $toggle.attr('aria-expanded', 'true');
+	            $panel.stop(true, true).slideDown(260);
+	        }
+	    });
+
 	});
 
 /* start preloader */
